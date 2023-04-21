a1bc483970afdef02e2c0167beb06956d77c167a
jay98patel <itsj10.jey@gmail.com>
2023-04-21 13:48:46 +0530

    implemented leaflet world map in home screen

--- src/app/simulator/simulator-routing.module.ts
+++ src/app/simulator/simulator-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SimulatorComponent } from './simulator.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'worldMap',
+    pathMatch: 'full',
+  },
+  {
+    path: 'worldMap',
+    component: SimulatorComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class SimulatorRoutingModule { }
+export class SimulatorRoutingModule {}
--- src/app/simulator/simulator.component.ts
+++ src/app/simulator/simulator.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+import 'leaflet-sidebar-v2';
 
 @Component({
   selector: 'app-simulator',
   templateUrl: './simulator.component.html',
-  styleUrls: ['./simulator.component.scss']
+  styleUrls: ['./simulator.component.scss'],
 })
 export class SimulatorComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.generateMap();
   }
 
+  generateMap() {
+    const maxBoundsViscocity = 1.0;
+    const map = L.map('map', {
+      maxBoundsViscosity: maxBoundsViscocity,
+    }).setView([0, 0], 1);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+      maxZoom: 18,
+      tileSize: 512,
+      zoomOffset: -1,
+    }).addTo(map);
+
+    map.fitWorld();
+  }
 }
